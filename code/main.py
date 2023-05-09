3e508dcf4ff6273cb6740e16f66be507b5012cd6
Minseok Kwak <kwak@minseok.me>
2023-05-09 06:06:22 +0000

    Feature[Model][Train]: Add config parser in training mode.
    - Add config.conf file.
    - Add config parser in main.
    - closes #13 Due to add feature.

--- code/main.py
+++ code/main.py
@@ -2,11 +2,12 @@ import argparse
 
 from train import *
 from inference import *
+import configparser
 
-def main(args):
+def main(args, config=None):
     if args.mode == "train" :
         print("Training mode")
-        train(args)
+        train(args, config)
         
     elif args.mode == "inference" :
         print("Inference mode")
@@ -14,10 +15,19 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument('--mode', type=str, default="train")
     parser.add_argument('--model_dir', type=str, default="./best_model")
     parser.add_argument('--model_name', type=str, default="klue/bert-base")
+    parser.add_argument('--config_path', type=str, default="")
 
     args = parser.parse_args()
-    main(args)
+
+    if args.config_path != "" :
+        config = configparser.ConfigParser()
+        config.read(args.config_path)
+        args.model_name = config['train']['model_name']
+
+        main(args, config)
+    else:
+        main(args)
--- code/train.py
+++ code/train.py
@@ -11,7 +11,24 @@ from metrics import *
 
 from CustomScheduler import CosineAnnealingWarmUpRestarts
 
-def train(args):
+def _getTrainerWithConfig(config):
+    return TrainingArguments(
+        output_dir                      = config["train"]["output_dir"],
+        save_total_limit                = int(config["train"]["save_total_limit"]),
+        save_steps                      = int(config["train"]["save_steps"]),
+        num_train_epochs                = int(config["train"]["num_train_epochs"]),
+        learning_rate                   = float(config["train"]["learning_rate"]),
+        per_device_eval_batch_size      = int(config["train"]["per_device_eval_batch_size"]),
+        warmup_steps                    = int(config["train"]["warmup_steps"]),
+        weight_decay                    = float(config["train"]["weight_decay"]),
+        logging_dir                     = config["train"]["logging_dir"],
+        logging_steps                   = int(config["train"]["logging_steps"]),
+        eval_steps                      = int(config["train"]["eval_steps"]),
+        evaluation_strategy             = config["train"]["evaluation_strategy"],
+        load_best_model_at_end          = config["train"]["load_best_model_at_end"]
+    )
+
+def train(args, config=None):
     MODEL_NAME = args.model_name
     
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
@@ -42,24 +59,26 @@ def train(args):
     scheduler = CosineAnnealingWarmUpRestarts(optimizers, T_0=1000, T_mult=2, eta_max=3e-5,  T_up=500, gamma=0.5)
 
     training_args = TrainingArguments(
-        output_dir='./results',          # output directory
-        save_total_limit=5,              # number of total save model.
-        save_steps=1,                 # model saving step. ## check-point가 여기야
-        num_train_epochs=5,              # total number of training epochs
-        learning_rate=5e-5,               # learning_rate
-        per_device_train_batch_size=16,  # batch size per device during training
-        per_device_eval_batch_size=16,   # batch size for evaluation
-        warmup_steps=1,                # number of warmup steps for learning rate scheduler
-        weight_decay=0.01,               # strength of weight decay
-        logging_dir='./logs',            # directory for storing logs
-        logging_steps=100,              # log saving step.
-        evaluation_strategy='steps', # evaluation strategy to adopt during training
-                                     # `no`: No evaluation during training.
-                                     # `steps`: Evaluate every `eval_steps`.
-                                     # `epoch`: Evaluate every end of epoch.
-        eval_steps = 500,            # evaluation step.
-        # load_best_model_at_end = True 
+        output_dir='./results',                     # output directory
+        save_total_limit=5,                         # number of total save model.
+        save_steps=500,                             # model saving step.
+        num_train_epochs=4,                         # total number of training epochs
+        learning_rate=0.001,                        # learning_rate
+        per_device_train_batch_size=16,             # batch size per device during training
+        per_device_eval_batch_size=16,              # batch size for evaluation
+        warmup_steps=500,                           # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,                          # strength of weight decay
+        logging_dir='./logs',                       # directory for storing logs
+        logging_steps=100,                          # log saving step.
+        evaluation_strategy='steps',                # evaluation strategy to adopt during training
+                                                        # `no`: No evaluation during training.
+                                                        # `steps`: Evaluate every `eval_steps`.
+                                                        # `epoch`: Evaluate every end of epoch.
+        eval_steps = 500,                            # evaluation step.
+        load_best_model_at_end = True 
     )
+
+    training_args = _getTrainerWithConfig(config) if config else None
     
     trainer = Trainer(
         model=model,                         # the instantiated 🤗 Transformers model to be trained
