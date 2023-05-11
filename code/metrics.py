39e7b0704fafa58b0d34153cec3205b4557dd9ca
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-05-11 16:27:23 +0900

    Feature: add more metrics (#31)
    * Feature: add seperate_f1_score
    
    30개의 라벨별로 별도의 f1 score를 출력합니다.
    
    * Feature: add draw_confusion_matrix
    
    더 나은 시각화를 위해 30라벨의 혼동행렬을 print합니다.
    
    * Feature: change return of seperate_f1
    
    precision, recall은 혼동행렬이 있기 때문에 불필요한 듯해서
    return에서 제외합니다.

--- code/metrics.py
+++ code/metrics.py
@@ -1,6 +1,6 @@
 import sklearn
 import numpy as np
-
+import sklearn.metrics
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 
 def klue_re_micro_f1(preds, labels):
@@ -20,6 +20,60 @@ def klue_re_micro_f1(preds, labels):
     label_indices = list(range(len(label_list)))
     label_indices.remove(no_relation_label_idx)
     return sklearn.metrics.f1_score(labels, preds, average="micro", labels=label_indices) * 100.0
+
+def klue_re_seperate_f1(preds, labels):
+    """KLUE-RE seperate f1 (including no_relation)
+    
+    출력물은 #precision, #recall, f1-score, support(=개수)로 2개입니다.
+    """
+    label_list = ['no_relation', 'org:top_members/employees', 'org:members',
+       'org:product', 'per:title', 'org:alternate_names',
+       'per:employee_of', 'org:place_of_headquarters', 'per:product',
+       'org:number_of_employees/members', 'per:children',
+       'per:place_of_residence', 'per:alternate_names',
+       'per:other_family', 'per:colleagues', 'per:origin', 'per:siblings',
+       'per:spouse', 'org:founded', 'org:political/religious_affiliation',
+       'org:member_of', 'per:parents', 'org:dissolved',
+       'per:schools_attended', 'per:date_of_death', 'per:date_of_birth',
+       'per:place_of_birth', 'per:place_of_death', 'org:founded_by',
+       'per:religion']
+    # no_relation_label_idx = label_list.index("no_relation")
+    label_indices = list(range(len(label_list)))
+    # label_indices.remove(no_relation_label_idx)
+    metric = sklearn.metrics.precision_recall_fscore_support(
+        labels, preds, average=None, labels=label_indices)
+    metric_label = ['precision', 'recall', 'f1-score', 'support']
+    ans = [ (metric_label[i], list(metric[i])) for i in range(len(metric_label)) ]
+    ans[2] = (ans[2][0], [ (label_list[i], v) for i, v in enumerate(ans[2][1]) ] )
+    ans[3] = (ans[3][0], [ (label_list[i], v) for i, v in enumerate(ans[3][1]) ] )
+    ans.pop(0)
+    ans.pop(0)
+    return ans
+
+
+def draw_cofusion_matrix(preds, labels):
+    label_list = ['no_relation', 'org:top_members/employees', 'org:members',
+        'org:product', 'per:title', 'org:alternate_names',
+        'per:employee_of', 'org:place_of_headquarters', 'per:product',
+        'org:number_of_employees/members', 'per:children',
+        'per:place_of_residence', 'per:alternate_names',
+        'per:other_family', 'per:colleagues', 'per:origin', 'per:siblings',
+        'per:spouse', 'org:founded', 'org:political/religious_affiliation',
+        'org:member_of', 'per:parents', 'org:dissolved',
+        'per:schools_attended', 'per:date_of_death', 'per:date_of_birth',
+        'per:place_of_birth', 'per:place_of_death', 'org:founded_by',
+        'per:religion']
+    mat = sklearn.metrics.confusion_matrix(
+        y_true=labels, y_pred=preds, normalize='true')
+    mat = np.array([list(int('{:2.0f}'.format((x*99))) for x in xx) for xx in mat])
+    #0.02로 적힌 숫자를 100을 곱해서 2로 만들어주고 내림해줍니다.
+    np.set_printoptions(linewidth=150) 
+    for i in range(len(mat)):
+        print(mat[i], end=' ')
+        print(label_list[i])
+    np.set_printoptions(linewidth=75)
+    return
+
 
 def klue_re_auprc(probs, labels):
     """KLUE-RE AUPRC (with no_relation)"""
@@ -43,9 +97,12 @@ def compute_metrics(pred):
   f1 = klue_re_micro_f1(preds, labels)
   auprc = klue_re_auprc(probs, labels)
   acc = accuracy_score(labels, preds) # 리더보드 평가에는 포함되지 않습니다.
+  seperate_f1 = klue_re_seperate_f1(preds, labels)
+  draw_cofusion_matrix(preds, labels)
 
   return {
       'micro f1 score': f1,
       'auprc' : auprc,
       'accuracy': acc,
+      'seperate f1 score': seperate_f1
   }
