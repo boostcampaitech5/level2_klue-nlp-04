5c865f54dc8f40861ba7312f3ecdcb5922a09c18
Minseok Kwak <kwak@minseok.me>
2023-05-12 16:14:27 +0900

    [Feature][config] Add config options and fix bugs (#40)
    * [Feature][config] Add fp16, gradient_checkpointing options
    
    * [Feature][config] Add inference options in argument and config
    
    - Add saved_model_path, tokenizer_name and more options in argument.
    - Add inference options in config.conf.
    
    * [Fix][train] Fix trainer bug
    - Fix training_args to apply default training_args when config is None.
    
    * [Feature][config] Add and organize config options
    - Organize config options.
    - Add scheduler and dataset options in config parser.
    
    * [Fix][config] Fix to work boolean types.
    - Change to python style boolean.
    - Call values with function "getboolean".

--- code/main.py
+++ code/main.py
@@ -17,16 +17,23 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--mode', type=str, default="train")
-    parser.add_argument('--model_dir', type=str, default="./best_model")
     parser.add_argument('--model_name', type=str, default="klue/bert-base")
     parser.add_argument('--config_path', type=str, default="")
+
+    parser.add_argument('--model_path', type=str, default="./best_model")
+    parser.add_argument('--tokenizer_name', type=str, default="klue/bert-base")
+    parser.add_argument('--output_path', type=str, default="./prediction/submission.csv")
 
     args = parser.parse_args()
 
     if args.config_path != "" :
         config = configparser.ConfigParser()
         config.read(args.config_path)
-        args.model_name = config['train']['model_name']
+        args.model_name = config['model']['model_name']
+
+        args.model_path     = config['inference']['saved_model_path']
+        args.tokenizer_name = config['inference']['tokenizer_name']
+        args.output_path    = config['inference']['output_path']
 
         main(args, config)
     else:
