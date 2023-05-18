f273d1351eb7efb700237d3635229c6779da4429
Yungi <dbsrlskfdk@naver.com>
2023-05-18 17:23:23 +0900

    [Feature] Add Asymmetric Loss Class (#68)
    * [Feature] Add Asymmetric Loss Class
    - Add Asymmetric Loss Class Code in model.py
    
    * [Fix] Modify ASLoss Params
    - Modify Label numbers Param

--- code/model.py
+++ code/model.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 from transformers import AutoModel, AutoConfig, RobertaPreTrainedModel, ElectraPreTrainedModel
 
 
@@ -17,6 +19,56 @@ class FCLayer(nn.Module):
             x = self.tanh(x)
         return self.linear(x)
 
+class AsymmetricLoss(nn.Module):
+    """
+    Asymmetric Loss Function
+    References:
+        https://github.com/Alibaba-MIIL/ASL/blob/main/src/loss_functions/losses.py
+    """
+    def __init__(self, gamma_neg=4, gamma_pos=1, clip=0.05, eps=1e-8, disable_torch_grad_focal_loss=True):
+        super(AsymmetricLoss, self).__init__()
+
+        self.gamma_neg = gamma_neg
+        self.gamma_pos = gamma_pos
+        self.clip = clip
+        self.disable_torch_grad_focal_loss = disable_torch_grad_focal_loss
+        self.eps = eps
+
+    def forward(self, x, y):
+        """"
+        Parameters:
+            x: input logits
+            y: targets (multi-label binarized vector)
+        """
+
+        # Calculating Probabilities
+        x_sigmoid = torch.sigmoid(x)
+        xs_pos = x_sigmoid
+        xs_neg = 1 - x_sigmoid
+
+        # Asymmetric Clipping
+        if self.clip is not None and self.clip > 0:
+            xs_neg = (xs_neg + self.clip).clamp(max=1)
+
+        # Basic CE calculation
+        los_pos = y * torch.log(xs_pos.clamp(min=self.eps))
+        los_neg = (1 - y) * torch.log(xs_neg.clamp(min=self.eps))
+        loss = los_pos + los_neg
+
+        # Asymmetric Focusing
+        if self.gamma_neg > 0 or self.gamma_pos > 0:
+            if self.disable_torch_grad_focal_loss:
+                torch.set_grad_enabled(False)
+            pt0 = xs_pos * y
+            pt1 = xs_neg * (1 - y)  # pt = p if t > 0 else 1-p
+            pt = pt0 + pt1
+            one_sided_gamma = self.gamma_pos * y + self.gamma_neg * (1 - y)
+            one_sided_w = torch.pow(1 - pt, one_sided_gamma)
+            if self.disable_torch_grad_focal_loss:
+                torch.set_grad_enabled(True)
+            loss *= one_sided_w
+
+        return -loss.sum()
 
 class TwoEntityModel(RobertaPreTrainedModel):
     def __init__(self, config, args) -> None:
@@ -87,6 +139,7 @@ class RBertModel(RobertaPreTrainedModel):
         self.num_labels = config.num_labels
         self.SUBJ_TOKEN = args.SUBJ_TOKEN
         self.OBJ_TOKEN = args.OBJ_TOKEN
+        self.loss_fct = AsymmetricLoss()
 
         # # Entity Marker 사용
         # model.resize_token_embeddings(tokenizer.vocab_size + added_token_num) # 추가한 Special token 갯수만큼 Embedding을 늘려줘야함
@@ -157,8 +210,10 @@ class RBertModel(RobertaPreTrainedModel):
         outputs = (logits,) + output[2:] # logits, (hidden_states), (attentions)
 
         if labels is not None:
-            loss_fct = nn.CrossEntropyLoss()
-            loss = loss_fct(logits, labels)
+            # loss_fct = nn.CrossEntropyLoss()
+            # loss = loss_fct(logits, labels)
+            # For Asymmetric Loss
+            loss = self.loss_fct(logits, F.one_hot(labels, self.num_labels))
             outputs = (loss,) + outputs
 
         return outputs
